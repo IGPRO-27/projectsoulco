ff58fe8e53f611c78e8d6a014f1568d0fac867a4
Sidwise <sid_wise@hotmail.com>
2016-02-09 23:18:38 +0100

    adding fields to purchase report

--- __openerp__.py
+++ __openerp__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['project','sale','stock'],
+    'depends': ['project','sale','stock','purchase'],
 
     # always loaded
     'data': [
