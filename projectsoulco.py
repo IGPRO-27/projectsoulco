ce15143041ce06c3cf216cbc116c29df0ca67101
Adel27 <r-27adel@live.fr>
2016-02-13 11:45:10 +0100

    Onchange seriable field

--- projectsoulco.py
+++ projectsoulco.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-#from datetime import timedelta
 from openerp import models, fields, api, exceptions
 from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
 from datetime import datetime
+import logging
+_logger = logging.getLogger(__name__)
 
 class ProjectSoulcoTemplate(models.Model):
     _inherit = 'product.template'
@@ -11,6 +12,12 @@ class ProjectSoulcoTemplate(models.Model):
     ean13 = fields.Char(string ="Réference S" ,  required=True)
     default_code = fields.Char(string ="Réference L" ,  required=True)
     num_serie = fields.Boolean(string="Seriable")
+
+    @api.onchange('num_serie', 'track_all')
+    def on_num_serie_change(self):
+        if not self.track_all:
+            self.track_all = self.num_serie
+        _logger.debug("changed")
 
 class ProjectSoulcoProduct(models.Model):
     _inherit = 'product.product'
@@ -24,7 +31,6 @@ class stock_transfer_details(models.TransientModel):
     _inherit = 'stock.transfer_details'
 
     def default_get(self, cr, uid, fields, context=None):
-        print "DOOOOOOOOOOOOOOO TRANSFERRRRRRRRRRRRRRRRRRRRRTTTTTT"
         if context is None: context = {}
         res = super(stock_transfer_details, self).default_get(cr, uid, fields, context=context)
         picking_ids = context.get('active_ids', [])
@@ -48,7 +54,7 @@ class stock_transfer_details(models.TransientModel):
                     'packop_id': op.id,
                     'product_id': op.product_id.id,
                     'product_uom_id': op.product_uom_id.id,
-                    'quantity': 1,
+                    'quantity': 1, #op.product_qty
                     'package_id': op.package_id.id,
                     'lot_id': op.lot_id.id,
                     'sourceloc_id': op.location_id.id,
@@ -91,8 +97,62 @@ class stock_transfer_details(models.TransientModel):
                     packop_id = self.env['stock.pack.operation'].create(pack_datas)
                     processed_ids.append(packop_id.id)
         # Delete the others
-        # packops = self.env['stock.pack.operation'].search(['&', ('picking_id', '=', self.picking_id.id), '!', ('id', 'in', processed_ids)])
-        # packops.unlink()
+        packops = self.env['stock.pack.operation'].search(['&', ('picking_id', '=', self.picking_id.id), '!', ('id', 'in', processed_ids)])
+        packops.unlink()
         self.picking_id.do_transfer()
 
+        return True
+
+class ProjectSoulcoPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.cr_uid_ids_context
+    def do_transfer(self, cr, uid, picking_ids, context=None):
+        """
+            If no pack operation, we do simple action_done of the picking
+            Otherwise, do the pack operations
+        """
+        if not context:
+            context = {}
+        stock_move_obj = self.pool.get('stock.move')
+        for picking in self.browse(cr, uid, picking_ids, context=context):
+            if not picking.pack_operation_ids:
+                self.action_done(cr, uid, [picking.id], context=context)
+                continue
+            else:
+                need_rereserve, all_op_processed = self.picking_recompute_remaining_quantities(cr, uid, picking, context=context)
+                #create extra moves in the picking (unexpected product moves coming from pack operations)
+                todo_move_ids = []
+                if not all_op_processed:
+                    todo_move_ids += self._create_extra_moves(cr, uid, picking, context=context)
+
+                #split move lines if needed
+                toassign_move_ids = []
+                for move in picking.move_lines:
+                    remaining_qty = move.remaining_qty
+                    if move.state in ('done', 'cancel'):
+                        #ignore stock moves cancelled or already done
+                        continue
+                    elif move.state == 'draft':
+                        toassign_move_ids.append(move.id)
+                    if float_compare(remaining_qty, 0,  precision_rounding = move.product_id.uom_id.rounding) == 0:
+                        if move.state in ('draft', 'assigned', 'confirmed'):
+                            todo_move_ids.append(move.id)
+                    elif float_compare(remaining_qty,0, precision_rounding = move.product_id.uom_id.rounding) > 0 and \
+                                float_compare(remaining_qty, move.product_qty, precision_rounding = move.product_id.uom_id.rounding) < 0:
+                        new_move = stock_move_obj.split(cr, uid, move, remaining_qty, context=context)
+                        todo_move_ids.append(move.id)
+                        #Assign move as it was assigned before
+                        toassign_move_ids.append(new_move)
+                if need_rereserve or not all_op_processed:
+                    if not picking.location_id.usage in ("supplier", "production", "inventory"):
+                        self.rereserve_quants(cr, uid, picking, move_ids=todo_move_ids, context=context)
+                    self.do_recompute_remaining_quantities(cr, uid, [picking.id], context=context)
+                if todo_move_ids and not context.get('do_only_split'):
+                    self.pool.get('stock.move').action_done(cr, uid, todo_move_ids, context=context)
+                elif context.get('do_only_split'):
+                    context = dict(context, split=todo_move_ids)
+            self._create_backorder(cr, uid, picking, context=context)
+            if toassign_move_ids:
+                stock_move_obj.action_assign(cr, uid, toassign_move_ids, context=context)
         return True
