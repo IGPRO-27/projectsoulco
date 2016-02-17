73989162aacc1e4fcd01c8799feb2b7b9253be76
Adel27 <r-27adel@live.fr>
2016-02-17 12:13:38 +0100

    Unique serial number in stock.production.lot model

--- projectsoulco.py
+++ projectsoulco.py
@@ -10,8 +10,8 @@ _logger = logging.getLogger(__name__)
 class ProjectSoulcoTemplate(models.Model):
     _inherit = 'product.template'
 
-    ean13 = fields.Char(string ="Réference S" ,  required=True)
-    default_code = fields.Char(string ="Réference L" ,  required=True)
+    ean13 = fields.Char(string ="Réference S")
+    default_code = fields.Char(string ="Réference L")
     num_serie = fields.Boolean(string="Seriable")
 
     @api.onchange('num_serie', 'track_all')
@@ -28,136 +28,143 @@ class ProjectSoulcoProduct(models.Model):
 
     _constraints = [(_check_ean_key, 'override function', ['ean13'])]
 
+class ProjectSoulcoLot(models.Model):
+    _inherit = 'stock.production.lot'
+
+    _sql_constraints = [
+        ('serial_number', 'unique(name)', 'Serial number must be unique !'),
+    ]
+
 class stock_transfer_details(models.TransientModel):
     _inherit = 'stock.transfer_details'
 
-    def default_get(self, cr, uid, fields, context=None):
-        if context is None: context = {}
-        res = super(stock_transfer_details, self).default_get(cr, uid, fields, context=context)
-        picking_ids = context.get('active_ids', [])
-        active_model = context.get('active_model')
+    # def default_get(self, cr, uid, fields, context=None):
+    #     if context is None: context = {}
+    #     res = super(stock_transfer_details, self).default_get(cr, uid, fields, context=context)
+    #     picking_ids = context.get('active_ids', [])
+    #     active_model = context.get('active_model')
+    #
+    #     if not picking_ids or len(picking_ids) != 1:
+    #         # Partial Picking Processing may only be done for one picking at a time
+    #         return res
+    #     assert active_model in ('stock.picking'), 'Bad context propagation'
+    #     picking_id, = picking_ids
+    #     picking = self.pool.get('stock.picking').browse(cr, uid, picking_id, context=context)
+    #     print picking
+    #     items = []
+    #     packs = []
+    #     if not picking.pack_operation_ids:
+    #         picking.do_prepare_partial()
+    #     for op in picking.pack_operation_ids:
+    #         for n in range(1, int(op.product_qty)+1):
+    #             print n
+    #             item = {
+    #                 'packop_id': op.id,
+    #                 'product_id': op.product_id.id,
+    #                 'product_uom_id': op.product_uom_id.id,
+    #                 'quantity': 1, #op.product_qty
+    #                 'package_id': op.package_id.id,
+    #                 'lot_id': op.lot_id.id,
+    #                 'sourceloc_id': op.location_id.id,
+    #                 'destinationloc_id': op.location_dest_id.id,
+    #                 'result_package_id': op.result_package_id.id,
+    #                 'date': op.date,
+    #                 'owner_id': op.owner_id.id,
+    #             }
+    #             if op.product_id:
+    #                 items.append(item)
+    #             elif op.package_id:
+    #                 packs.append(item)
+    #     res.update(item_ids=items)
+    #     res.update(packop_ids=packs)
+    #     return res
 
-        if not picking_ids or len(picking_ids) != 1:
-            # Partial Picking Processing may only be done for one picking at a time
-            return res
-        assert active_model in ('stock.picking'), 'Bad context propagation'
-        picking_id, = picking_ids
-        picking = self.pool.get('stock.picking').browse(cr, uid, picking_id, context=context)
-        print picking
-        items = []
-        packs = []
-        if not picking.pack_operation_ids:
-            picking.do_prepare_partial()
-        for op in picking.pack_operation_ids:
-            for n in range(1, int(op.product_qty)+1):
-                print n
-                item = {
-                    'packop_id': op.id,
-                    'product_id': op.product_id.id,
-                    'product_uom_id': op.product_uom_id.id,
-                    'quantity': 1, #op.product_qty
-                    'package_id': op.package_id.id,
-                    'lot_id': op.lot_id.id,
-                    'sourceloc_id': op.location_id.id,
-                    'destinationloc_id': op.location_dest_id.id,
-                    'result_package_id': op.result_package_id.id,
-                    'date': op.date,
-                    'owner_id': op.owner_id.id,
-                }
-                if op.product_id:
-                    items.append(item)
-                elif op.package_id:
-                    packs.append(item)
-        res.update(item_ids=items)
-        res.update(packop_ids=packs)
-        return res
-
-    @api.one
-    def do_detailed_transfer(self):
-        processed_ids = []
-        # Create new and update existing pack operations
-        for lstits in [self.item_ids, self.packop_ids]:
-            for prod in lstits:
-                pack_datas = {
-                    'product_id': prod.product_id.id,
-                    'product_uom_id': prod.product_uom_id.id,
-                    'product_qty': prod.quantity,
-                    'package_id': prod.package_id.id,
-                    'lot_id': prod.lot_id.id,
-                    'location_id': prod.sourceloc_id.id,
-                    'location_dest_id': prod.destinationloc_id.id,
-                    'result_package_id': prod.result_package_id.id,
-                    'date': prod.date if prod.date else datetime.now(),
-                    'owner_id': prod.owner_id.id,
-                }
-                if prod.packop_id:
-                    prod.packop_id.with_context(no_recompute=True).write(pack_datas)
-                    processed_ids.append(prod.packop_id.id)
-                else:
-                    pack_datas['picking_id'] = self.picking_id.id
-                    packop_id = self.env['stock.pack.operation'].create(pack_datas)
-                    processed_ids.append(packop_id.id)
-
-        packops = self.env['stock.pack.operation'].search(['&', ('picking_id', '=', self.picking_id.id), '!', ('id', 'in', processed_ids)])
-        # print "Let's talk about %s" %  packops.
-        packops.unlink()
-        self.picking_id.do_transfer()
-
-        return True
+    # @api.one
+    # def do_detailed_transfer(self):
+    #     processed_ids = []
+    #     # Create new and update existing pack operations
+    #     for lstits in [self.item_ids, self.packop_ids]:
+    #         for prod in lstits:
+    #             pack_datas = {
+    #                 'product_id': prod.product_id.id,
+    #                 'product_uom_id': prod.product_uom_id.id,
+    #                 'product_qty': prod.quantity,
+    #                 'package_id': prod.package_id.id,
+    #                 'lot_id': prod.lot_id.id,
+    #                 'location_id': prod.sourceloc_id.id,
+    #                 'location_dest_id': prod.destinationloc_id.id,
+    #                 'result_package_id': prod.result_package_id.id,
+    #                 'date': prod.date if prod.date else datetime.now(),
+    #                 'owner_id': prod.owner_id.id,
+    #             }
+    #             if prod.packop_id:
+    #                 prod.packop_id.with_context(no_recompute=True).write(pack_datas)
+    #                 processed_ids.append(prod.packop_id.id)
+    #             else:
+    #                 pack_datas['picking_id'] = self.picking_id.id
+    #                 packop_id = self.env['stock.pack.operation'].create(pack_datas)
+    #                 processed_ids.append(packop_id.id)
+    #
+    #     packops = self.env['stock.pack.operation'].search(['&', ('picking_id', '=', self.picking_id.id), '!', ('id', 'in', processed_ids)])
+    #     # print "Let's talk about %s" %  packops.
+    #     packops.unlink()
+    #     self.picking_id.do_transfer()
+    #
+    #     return True
 
 class ProjectSoulcoPicking(models.Model):
     _inherit = 'stock.picking'
 
-    @api.cr_uid_ids_context
-    def do_transfer(self, cr, uid, picking_ids, context=None):
-        """
-            If no pack operation, we do simple action_done of the picking
-            Otherwise, do the pack operations
-        """
-        if not context:
-            context = {}
-        stock_move_obj = self.pool.get('stock.move')
-        for picking in self.browse(cr, uid, picking_ids, context=context):
-            if not picking.pack_operation_ids:
-                self.action_done(cr, uid, [picking.id], context=context)
-                continue
-            else:
-                need_rereserve, all_op_processed = self.picking_recompute_remaining_quantities(cr, uid, picking, context=context)
-                #create extra moves in the picking (unexpected product moves coming from pack operations)
-                todo_move_ids = []
-                if not all_op_processed:
-                    todo_move_ids += self._create_extra_moves(cr, uid, picking, context=context)
-
-                #split move lines if needed
-                toassign_move_ids = []
-                for move in picking.move_lines:
-                    remaining_qty = move.remaining_qty
-                    if move.state in ('done', 'cancel'):
-                        #ignore stock moves cancelled or already done
-                        continue
-                    elif move.state == 'draft':
-                        toassign_move_ids.append(move.id)
-
-                    if float_compare(remaining_qty, 0,  precision_rounding = move.product_id.uom_id.rounding) == 0:
-                        if move.state in ('draft', 'assigned', 'confirmed'):
-                            todo_move_ids.append(move.id)
-
-                    elif float_compare(remaining_qty,0, precision_rounding = move.product_id.uom_id.rounding) > 0 and \
-                         float_compare(remaining_qty, move.product_qty, precision_rounding = move.product_id.uom_id.rounding) < 0:
-                        print "I AAAAAAAAAAAAAAAAAAMMMMMMMMMMMM HERRRRRRRRRRRRRE"
-                        new_move = stock_move_obj.split(cr, uid, move, remaining_qty, context=context)
-                        todo_move_ids.append(move.id)
-                        #Assign move as it was assigned before
-                        toassign_move_ids.append(new_move)
-                if need_rereserve or not all_op_processed:
-                    if not picking.location_id.usage in ("supplier", "production", "inventory"):
-                        self.rereserve_quants(cr, uid, picking, move_ids=todo_move_ids, context=context)
-                    self.do_recompute_remaining_quantities(cr, uid, [picking.id], context=context)
-                if todo_move_ids and not context.get('do_only_split'):
-                    self.pool.get('stock.move').action_done(cr, uid, todo_move_ids, context=context)
-                elif context.get('do_only_split'):
-                    context = dict(context, split=todo_move_ids)
-            self._create_backorder(cr, uid, picking, context=context)
-            if toassign_move_ids:
-                stock_move_obj.action_assign(cr, uid, toassign_move_ids, context=context)
-        return True
+    # @api.cr_uid_ids_context
+    # def do_transfer(self, cr, uid, picking_ids, context=None):
+    #     """
+    #         If no pack operation, we do simple action_done of the picking
+    #         Otherwise, do the pack operations
+    #     """
+    #     if not context:
+    #         context = {}
+    #     stock_move_obj = self.pool.get('stock.move')
+    #     for picking in self.browse(cr, uid, picking_ids, context=context):
+    #         if not picking.pack_operation_ids:
+    #             self.action_done(cr, uid, [picking.id], context=context)
+    #             continue
+    #         else:
+    #             need_rereserve, all_op_processed = self.picking_recompute_remaining_quantities(cr, uid, picking, context=context)
+    #             #create extra moves in the picking (unexpected product moves coming from pack operations)
+    #             todo_move_ids = []
+    #             if not all_op_processed:
+    #                 todo_move_ids += self._create_extra_moves(cr, uid, picking, context=context)
+    #
+    #             #split move lines if needed
+    #             toassign_move_ids = []
+    #             for move in picking.move_lines:
+    #                 remaining_qty = move.remaining_qty
+    #                 if move.state in ('done', 'cancel'):
+    #                     #ignore stock moves cancelled or already done
+    #                     continue
+    #                 elif move.state == 'draft':
+    #                     toassign_move_ids.append(move.id)
+    #
+    #                 if float_compare(remaining_qty, 0,  precision_rounding = move.product_id.uom_id.rounding) == 0:
+    #                     if move.state in ('draft', 'assigned', 'confirmed'):
+    #                         todo_move_ids.append(move.id)
+    #
+    #                 elif float_compare(remaining_qty,0, precision_rounding = move.product_id.uom_id.rounding) > 0 and \
+    #                      float_compare(remaining_qty, move.product_qty, precision_rounding = move.product_id.uom_id.rounding) < 0:
+    #                     print "I AAAAAAAAAAAAAAAAAAMMMMMMMMMMMM HERRRRRRRRRRRRRE"
+    #                     new_move = stock_move_obj.split(cr, uid, move, remaining_qty, context=context)
+    #                     todo_move_ids.append(move.id)
+    #                     #Assign move as it was assigned before
+    #                     toassign_move_ids.append(new_move)
+    #             if need_rereserve or not all_op_processed:
+    #                 if not picking.location_id.usage in ("supplier", "production", "inventory"):
+    #                     self.rereserve_quants(cr, uid, picking, move_ids=todo_move_ids, context=context)
+    #                 self.do_recompute_remaining_quantities(cr, uid, [picking.id], context=context)
+    #             if todo_move_ids and not context.get('do_only_split'):
+    #                 self.pool.get('stock.move').action_done(cr, uid, todo_move_ids, context=context)
+    #             elif context.get('do_only_split'):
+    #                 context = dict(context, split=todo_move_ids)
+    #         self._create_backorder(cr, uid, picking, context=context)
+    #         if toassign_move_ids:
+    #             stock_move_obj.action_assign(cr, uid, toassign_move_ids, context=context)
+    #     return True
